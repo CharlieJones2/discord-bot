be45e9794a97991b1f207f9e722fad5af435e8d1
Charlie Jones <125832247+CharlieJones2@users.noreply.github.com>
2023-08-29 15:56:53 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,12 +17,16 @@ def run_bot():
                                                         scope='playlist-modify-public playlist-modify-private '
                                                               'user-read-currently-playing'))
 
-    bot = commands.Bot(command_prefix='!', intents=ints, permissions=2147535872)
+        bot = commands.Bot(command_prefix='!', intents=ints, permissions=2147535872)
     name = 'Playlist Creator'
 
     @bot.event
+# initiate bot
     async def on_ready():
         synced = await bot.tree.sync()
+
+# display number of active commands the bot can use
+
         print(f'Logged in as {name}!\n'
               f'synced {len(synced)} command(s)')
 
@@ -37,50 +41,87 @@ def run_bot():
                                                 'Defaults to the current song if no link is provided.')
 
     @bot.tree.command(name='playlist')
+# initiate command
     async def playlist(interaction: discord.Interaction):
+
+        # send message with playlist link
+
         await interaction.response.send_message('Here is the link to your playlist :)\n\n'
                                                 'https://open.spotify.com/playlist/...')
 
     @bot.tree.command(name='cover')
     @app_commands.describe(link='provide the link to the song or album you want the cover of')
+# initiate command and prevent the bot responding to itself
     async def cover(interaction: discord.Interaction, link: typing.Optional[str]):
         if interaction.user == bot.user:
             return
+
+# defaults to current song if no link is provided
+
         if link is None:
             track = spotify.current_playback()
-            album_cover = track["item"]["album"]["images"][0]["url"]
-            await interaction.response.send_message(album_cover)
+            if track is None:
+                await interaction.response.send_message('You aren\'t currently playing anything! Start playing a '
+                                                        'song and try again, or use the `link` parameter to '
+                                                        'specify the cover you want to view by pasting the spotify '
+                                                        'link')
+            else:
+                album_cover = track["item"]["album"]["images"][0]["url"]
+                await interaction.response.send_message(album_cover)
+
+# parse message for link and extract details
+
         else:
             if 'open.spotify.com/track/' in link:
                 track_id = link.split('open_spotify.com/track/*?')[0].split('/track/')[1].split('?')[0]
                 spotify_uri = f'spotify:track:{track_id}'
                 track = spotify.track(spotify_uri)
+
+                # return album cover
+
                 await interaction.response.send_message(track["album"]["images"][0]["url"])
             elif 'open.spotify.com/album/' in link:
                 album_id = link.split('open_spotify.com/album/')[0].split('/album/')[1].split('?')[0]
                 spotify_uri = f'spotify:album:{album_id}'
                 album = spotify.album(spotify_uri)
+
+                # return album cover
+
                 await interaction.response.send_message(album["images"][0]["url"])
             else:
                 pass
 
     @bot.tree.command(name='song')
     @app_commands.describe(link='provide the link to your song')
+# initiate command and prevent bot responding to itself
     async def song(interaction: discord.Interaction, link: typing.Optional[str]):
-        print(interaction.user)
         if interaction.user == bot.user:
             return
+
+# defaults to current song if no link is provided
+
         if link is None:
             track = spotify.current_playback()
-            spotify_uri = track["item"]["uri"]
-            track = spotify.track(spotify_uri)
-            playlist_id = 'https://open.spotify.com/playlist/...'
-            spotify.playlist_add_items(playlist_id, items=[spotify_uri])
-            await interaction.response.send_message(f'Successfully added the following track to your playlist: \n\n'
-                                                    f'Song: `{track["name"]}`\n'
-                                                    f'Artist: `{track["artists"][0]["name"]}`\n\n'
-                                                    f'{track["album"]["images"][0]["url"]}')
-        # parse message for spotify link
+            if track is None:
+                await interaction.response.send_message('You aren\'t currently playing anything! Start playing a '
+                                                        'song and try again, or use the `link` parameter to '
+                                                        'specify the song you want to add by pasting the spotify '
+                                                        'link')
+            else:
+                spotify_uri = track["item"]["uri"]
+                track = spotify.track(spotify_uri)
+                playlist_id = 'https://open.spotify.com/playlist/...'
+                spotify.playlist_add_items(playlist_id, items=[spotify_uri])
+
+# send response confirming track has been added
+
+                await interaction.response.send_message(f'Successfully added the following track to your playlist: \n\n'
+                                                        f'Song: `{track["name"]}`\n'
+                                                        f'Artist: `{track["artists"][0]["name"]}`\n\n'
+                                                        f'{track["album"]["images"][0]["url"]}')
+
+# parse message for link and extract details
+
         else:
             if 'open.spotify.com/track/' in link:
                 track_id = link.split('open_spotify.com/track/*?')[0].split('/track/')[1].split('?')[0]
@@ -88,6 +129,8 @@ def run_bot():
                 track = spotify.track(spotify_uri)
                 playlist_id = 'https://open.spotify.com/playlist/...'
                 spotify.playlist_add_items(playlist_id, items=[spotify_uri])
+
+# send response confirming track has been added
 
                 await interaction.response.send_message(f'Successfully added the following track to your playlist: \n\n'
                                                         f'Song: `{track["name"]}`\n'
